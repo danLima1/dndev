3adf8eec2a0a58d3a23ad3784c10fc824dab1dce
danLima1 <danieldelimamendes500@gmail.com>
2023-04-05 21:32:34 -0300

    mudanças js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 $(document).ready(myApp)
 
-function myApp(){
-console.log('funciona!')
+function myApp() {
+    // carrega a página inicial
+    loadpage('contacts')
 
 
+
+}
+// carrega uma página
+function loadpage(page) {
+
+    // montar os caminhos para os componentes da pagina solicitada.
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `/pages/${page}/index.css`,
+        js: `pages/${page}/index.js`
+    }
+    $.get(path.html).done((data) => {
+        $('main').html(data)
+    })
+        .fail((error) => {
+            loadpage('e404')
+
+        })
 }
